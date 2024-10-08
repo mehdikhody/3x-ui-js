4bd5dd5c0161a793ce57dc85e1afff26552203d0
Mehdikhody <khodayari.khoram@gmail.com>
2024-10-08 15:06:24 +0330

    refactor npm-publish workflow.

--- tests/utils/api.ts
+++ tests/utils/api.ts
@@ -1,4 +1,4 @@
 import { XuiApi } from "3x-ui";
 
-const webBasePath = "SzbexLiMYy";
+const webBasePath = "O3dyelGTRl";
 export const api = new XuiApi(`http://admin:admin@localhost:2053/${webBasePath}`);
